5d9d000c48576bf9c8dfb947d42d2faaaa67ef79
Oscar Shoudt <shoudto@gmail.com>
2019-05-29 00:07:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,12 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9 
+    SQL
     
+    DB[:conn].execute(sql)
   end 
 end
