5c662cb716bb6df6a2781020e2bd2d0d30c3b0ad
Oscar Shoudt <shoudto@gmail.com>
2019-05-29 00:18:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,7 @@ class Student
     end
   end 
   
-  def self.first_X_students_in_grade_10(grade)
+  def self.first_X_students_in_grade_10(student)
       sql = <<-SQL
       SELECT *
       FROM students 
