8650ca5236d2ca0ea3f8bf7f23940384315f75d9
Oscar Shoudt <shoudto@gmail.com>
2019-05-29 00:25:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,7 +101,7 @@ class Student
       WHERE grade = 10 
     SQL
     
-     DB[:conn].execute(sql, name).map do |row| 
+     DB[:conn].execute(sql).map do |row| 
       self.new_from_db(row)
     end.first 
   end 
